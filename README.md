4a7c8d9e0896fac3d83cae13575803918635de49
Flávio Escobar <flavioescobar93@gmail.com>
2026-08-10 21:58:51 +0000

    chore: config indent with `prettier`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 É um clone do tabnews para estudo do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Clone TabNews - Testando o lint</h1>
+  return <h1>Clone TabNews - Testando o lint</h1>;
+}
+
+function test() {
+  console.log("Testando o lint");
 }
 
 export default Home;
