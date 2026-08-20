6ae17206ad82e252ffb6f6bb1a6dd93c23261c28
Flávio Escobar <flavioescobar93@gmail.com>
2026-08-20 03:39:35 +0000

    minha versão da implementação de retornar a versão do postgres

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,22 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "Status Ok" });
+  const updateAt = new Date();
+  const postgresVersion = await database.query("SHOW server_version;"); //uma query é uma promessa
+  const postgresConnections = await database.query(
+    "SELECT current_setting('max_connections')::int AS max_connections, COUNT(*)::int AS used_connections FROM pg_stat_activity;",
+  );
+
+  response.status(200).json({
+    update_at: updateAt.toISOString(),
+    postgresVersion: parseFloat(postgresVersion.rows[0].server_version),
+    postgresMaxConnections: parseInt(
+      postgresConnections.rows[0].max_connections,
+    ),
+    postgresUsedConnections: parseInt(
+      postgresConnections.rows[0].used_connections,
+    ),
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,15 @@
 test("GET to api/v1/status should return 200 and the correct status information", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
-  console.log(response);
+
+  const responseBody = await response.json();
+  expect(responseBody.update_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parsedUpdatedAt);
+  expect(Number.isFinite(responseBody.postgresVersion)).toBe(true);
+  expect(Number.isInteger(responseBody.postgresMaxConnections)).toBe(true);
+  expect(Number.isInteger(responseBody.postgresUsedConnections)).toBe(true);
+
+  console.log(responseBody);
 });
