a71948c5839e7a524dcb49a209494aea876da67e
Flávio Escobar <flavioescobar93@gmail.com>
2026-08-21 18:52:05 +0000

    adicionando logds de erro

--- infra/database.js
+++ infra/database.js
@@ -6,13 +6,23 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DATABASE,
+    database: process.env.POSTGRES_DB,
   });
-  await client.connect();
+  console.log("Credenciais do postgres:", {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  });
+
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
+    console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
