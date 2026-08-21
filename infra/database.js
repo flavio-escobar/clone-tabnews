f3916ca275fd178b6c731c2dcf8bb803adc096d0
Flávio Escobar <flavioescobar93@gmail.com>
2026-08-21 21:25:32 +0000

    adding error handling certficate

--- infra/database.js
+++ infra/database.js
@@ -7,10 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
-    ssl:
-      process.env.NODE_ENV === "development"
-        ? { rejectUnauthorized: false }
-        : true,
+    ssl: getSSLValues(),
   });
   console.log("Credenciais do postgres:", {
     host: process.env.POSTGRES_HOST,
@@ -35,3 +32,13 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  } else {
+    return process.env.NODE_ENV === "development" ? false : true;
+  }
+}
