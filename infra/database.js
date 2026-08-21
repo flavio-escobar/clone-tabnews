6d5a71c143b18ea60b0ce686a966ca97380d0c17
Flávio Escobar <flavioescobar93@gmail.com>
2026-08-21 20:04:23 +0000

    fix: ssl connection with database

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,10 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
+    ssl:
+      process.env.NODE_ENV === "development"
+        ? { rejectUnauthorized: false }
+        : true,
   });
   console.log("Credenciais do postgres:", {
     host: process.env.POSTGRES_HOST,
