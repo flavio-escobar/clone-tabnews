3b03356b7250585b3a0f80aec6e41819442b047d
flavio-escobar <flavioescobar93@gmail.com>
2026-08-22 23:42:19 -0300

    implementing tests for migration

--- tests/integration/api/v1/migrations/post.test.js
+++ tests/integration/api/v1/migrations/post.test.js
@@ -1,6 +1,9 @@
 test("POST to api/v1/migration should return 200 and the correct status information", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations");
+  const response = await fetch("http://localhost:3000/api/v1/migrations",{
+    method: 'POST'
+  });
   expect(response.status).toBe(200);
+
   const responseBody = await response.json();
   console.log(responseBody);
   expect(Array.isArray(responseBody)).toBe(true);
