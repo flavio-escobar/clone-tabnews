0c07860edd691d6af43c5b5cc8b6f7a7bfe43f33
flavio-escobar <flavioescobar93@gmail.com>
2026-08-24 23:52:39 -0300

    estruturação para tratar metodos http no migrations

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,12 +3,15 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
-  if (request.method != "GET" && request.method != "POST") {
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
     return response.status(405).json({
-      error: "O metodo HTTP enviado nao e suportado para este recurso.",
+      error: `O metodo "${request.method}" enviado nao e suportado para este recurso.`,
     });
-  } else {
-    const dbClient = await database.getNewClient();
+  }
+  let dbClient;
+  try {
+    dbClient = await database.getNewClient();
     const defaultMigrationOptions = {
       dbClient: dbClient,
       dryRun: true,
@@ -17,35 +20,26 @@ export default async function migrations(request, response) {
       verbose: true,
       migrationsTable: "pgmigrations",
     };
-    try {
-      if (request.method === "GET") {
-        console.log("Entrou no GET");
-        console.log("NEXT.JS env: ", process.env.NODE_ENV);
-        console.log("DB_PASS env: ", process.env.POSTGRES_PASSWORD);
-        const pendingMigrations = await migrationRunner(
-          defaultMigrationOptions,
-        );
-        await dbClient.end();
-        return response.status(200).json(pendingMigrations);
-      }
-      if (request.method === "POST") {
-        console.log("Entrou no POST");
-        const migratedMigrations = await migrationRunner({
-          ...defaultMigrationOptions,
-          dryRun: false,
-        });
-        await dbClient.end();
-        if (migratedMigrations.length > 0) {
-          return response.status(201).json(migratedMigrations);
-        } else {
-          return response.status(200).json(migratedMigrations);
-        }
-      }
-    } catch (error) {
-      throw error;
-    } finally {
-      await dbClient.end();
+
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+
+      return response.status(200).json(pendingMigrations);
     }
+    if (request.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      });
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json(migratedMigrations);
+      }
+      return response.status(200).json(migratedMigrations);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    await dbClient.end();
   }
-  return response.status(405).end();
 }
