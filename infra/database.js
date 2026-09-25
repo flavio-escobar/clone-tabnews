270e51c2044893b86a6ef0a084064d5d24c92e09
flavio-escobar <flavioescobar93@gmail.com>
2026-09-24 23:32:56 -0300

    feat: use ServiceError in database.js

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,5 @@
 import { Client } from "pg";
+import { ServiceError } from "./errors.js";
 
 async function query(queryObject) {
   //console.log("Credenciais do postgres:", {
@@ -14,9 +15,11 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.log("\n Error dentro do catch do database.js:");
-    console.error(error);
-    throw error;
+    const serviceErrorObject = new ServiceError({
+      message: "Erro na conexão com o banco de dados ou na query SQL.",
+      cause: error,
+    });
+    throw serviceErrorObject;
   } finally {
     await client?.end();
   }
